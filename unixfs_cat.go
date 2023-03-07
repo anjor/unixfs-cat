8d83efe33248d3dc358c5dead496cef509c030b9
anjor <anjor@umd.edu>
2023-03-07 22:30:27 +0000

    initial

---
+++ unixfs_cat.go
@@ -0,0 +1,40 @@
+package unixfs_cat
+
+import (
+	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-ipld-format"
+	"github.com/ipfs/go-merkledag"
+	"github.com/ipfs/go-unixfs"
+	unixfspb "github.com/ipfs/go-unixfs/pb"
+	mh "github.com/multiformats/go-multihash"
+)
+
+func ConcatNodes(nodes ...*merkledag.ProtoNode) (*merkledag.ProtoNode, error) {
+	nd := unixfs.NewFSNode(unixfspb.Data_File)
+	var links []format.Link
+
+	for _, node := range nodes {
+		s := uint64(len(node.Data()))
+		links = append(links, format.Link{
+			Name: "",
+			Cid:  node.Cid(),
+			Size: s,
+		})
+
+		nd.AddBlockSize(s)
+	}
+	ndb, err := nd.GetBytes()
+	if err != nil {
+		return nil, err
+	}
+	pbn := merkledag.NodeWithData(ndb)
+	pbn.SetCidBuilder(cid.V1Builder{MhType: uint64(mh.SHA2_256)})
+
+	for _, l := range links {
+		err := pbn.AddRawLink("", &l)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return pbn, nil
+}
---
+++ unixfs_cat_test.go
@@ -0,0 +1,39 @@
+package unixfs_cat
+
+import (
+	"bytes"
+	"github.com/ipfs/go-merkledag"
+	"testing"
+)
+
+func TestConcatNodes(t *testing.T) {
+	node1 := merkledag.NodeWithData([]byte("hello"))
+	node2 := merkledag.NodeWithData([]byte("world!"))
+
+	parent, err := ConcatNodes(node1, node2)
+	if err != nil {
+		t.Fatal("concat failed", err)
+	}
+	m, err := parent.MarshalJSON()
+	if err != nil {
+		t.Fatal("failed to marshal", err)
+	}
+
+	n1, err := node1.MarshalJSON()
+	if err != nil {
+		t.Fatal("failed to marshal", err)
+	}
+
+	n2, err := node2.MarshalJSON()
+	if err != nil {
+		t.Fatal("failed to marshal", err)
+	}
+
+	t.Logf("parent := %v", bytes.NewBuffer(n1).String())
+	t.Logf("parent := %v", bytes.NewBuffer(n2).String())
+	t.Logf("parent := %v", bytes.NewBuffer(m).String())
+
+	t.Logf("parent := %v", node1.String())
+	t.Logf("parent := %v", node2.String())
+	t.Logf("parent := %v", parent.String())
+}
