1693a9d6a982531ff9a1716ba791791cb2f0de73
anjor <anjor@umd.edu>
2023-05-09 10:32:23 +0100

    rename file specific

--- unixfs_cat.go
+++ unixfs_cat.go
@@ -1,8 +1,6 @@
 package unixfs_cat
 
 import (
-	"errors"
-	"fmt"
 	ipld "github.com/ipfs/go-ipld-format"
 	"github.com/ipfs/go-merkledag"
 	"github.com/ipfs/go-unixfs"
@@ -18,12 +16,12 @@ type ParentDagBuilder struct {
 	maxLinks int
 }
 
-func (pdb ParentDagBuilder) ConcatNodes(nodes ...ipld.Node) ([]*merkledag.ProtoNode, error) {
+func (pdb ParentDagBuilder) ConcatFileNodes(nodes ...ipld.Node) ([]*merkledag.ProtoNode, error) {
 	var pbns []*merkledag.ProtoNode
 	ndwl := nodeWithLinks{node: unixfs.NewFSNode(unixfspb.Data_File)}
 	for _, node := range nodes {
 		if len(ndwl.node.BlockSizes()) < pdb.maxLinks {
-			if err := ndwl.addLink(node); err != nil {
+			if err := ndwl.concatFileNode(node); err != nil {
 				return nil, err
 			}
 		} else {
@@ -35,7 +33,7 @@ func (pdb ParentDagBuilder) ConcatNodes(nodes ...ipld.Node) ([]*merkledag.ProtoN
 			pbns = append(pbns, pbn)
 
 			ndwl = nodeWithLinks{node: unixfs.NewFSNode(unixfspb.Data_File)}
-			if err := ndwl.addLink(node); err != nil {
+			if err := ndwl.concatFileNode(node); err != nil {
 				return nil, err
 			}
 
@@ -50,56 +48,4 @@ func (pdb ParentDagBuilder) ConcatNodes(nodes ...ipld.Node) ([]*merkledag.ProtoN
 	pbns = append(pbns, pbn)
 
 	return pbns, nil
-}
-
-func (ndwl *nodeWithLinks) constructPbNode() (pbn *merkledag.ProtoNode, err error) {
-	ndb, err := ndwl.node.GetBytes()
-	if err != nil {
-		return
-	}
-
-	pbn = merkledag.NodeWithData(ndb)
-
-	for _, l := range ndwl.links {
-		err = pbn.AddRawLink("", &l)
-		if err != nil {
-			return
-		}
-	}
-
-	return
-}
-
-func (ndwl *nodeWithLinks) addLink(node ipld.Node) error {
-
-	switch node := node.(type) {
-
-	case *merkledag.RawNode:
-		s := len(node.RawData())
-
-		ndwl.links = append(ndwl.links, ipld.Link{Name: "", Cid: node.Cid()})
-		ndwl.node.AddBlockSize(uint64(s))
-
-	case *merkledag.ProtoNode:
-		un, err := unixfs.ExtractFSNode(node)
-		if err != nil {
-			return err
-		}
-
-		switch t := un.Type(); t {
-		case unixfs.TRaw, unixfs.TFile:
-		default:
-			return errors.New(fmt.Sprintf("can only concat raw or file types, instead found %s", t))
-		}
-
-		s := un.FileSize()
-
-		ndwl.links = append(ndwl.links, ipld.Link{Name: "", Cid: node.Cid()})
-		ndwl.node.AddBlockSize(s)
-
-	default:
-		return errors.New("unknown node")
-	}
-
-	return nil
 }
